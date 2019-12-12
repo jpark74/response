635e3324dd44741422be57cd3261854c5c1d5895
jpark74 <54952591+jpark74@users.noreply.github.com>
2019-12-12 13:21:43 -0500

    Add files via upload

---
+++ Reading/scripts.js
@@ -0,0 +1,9 @@
+let toggles = document.querySelectorAll('.toggle')
+
+for (let i = 0; i < toggles.length; i++) { 
+  toggles[i].addEventListener('click', function() {
+    this.classList.toggle('show')
+  })
+}
+
+    
